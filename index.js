7ba53d693adbadf1ed6ab09a64438f451836a42a
zl90 <101400977+zl90@users.noreply.github.com>
2022-06-09 22:09:21 +1000

    Add file serving and links

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import http from "http";
 import "dotenv/config";
+import fs from "fs";
 
 // Grab the port number from environment
 const port = process.env.PORT || 8080;
@@ -8,5 +9,44 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
-  res.end();
+
+  // When we receive a http request, check the URL,
+  // then serve HTML accordingly.
+  switch (req.url) {
+    case "/index.html":
+    case "/home":
+    case "/":
+      res.end(readHTML("./index.html"));
+      break;
+    case "/about":
+    case "/about.html":
+      res.end(readHTML("./about.html"));
+      break;
+    case "/contact-me":
+    case "/contact-me.html":
+      res.end(readHTML("./contact-me.html"));
+      break;
+    default:
+      res.end(readHTML("./404.html"));
+      break;
+  }
+});
+
+// Grab the contents of the html file for serving.
+const readHTML = (filename) => {
+  let result;
+
+  try {
+    const data = fs.readFileSync(filename, "utf8");
+    result = data;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return result;
+};
+
+// Activate the server.
+server.listen(port, () => {
+  console.log("Server is listening...");
 });
